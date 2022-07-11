f38009a02ac433b09722cb575393e43c89fab7a0
sreelekha-2 <sreelekhakutikanti@gmail.com>
2022-07-11 13:33:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-dhm22r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-dhm22r)
---
+++ script.js
@@ -0,0 +1,31 @@
+console.log('Hello!');
+
+const arr = [3, 8, 7, 6, 5, -4, 3, 2, 1];
+const ascendingOrderSortedArr = arr.sort();
+console.log(ascendingOrderSortedArr);
+
+const descendingOrderSortedArr = arr.sort().reverse();
+console.log(descendingOrderSortedArr);
+
+console.log(arr);
+const arr2 = [
+  [1, 2, 1, 24],
+  [8, [11, [9, [1, 7], 2], 4]],
+  [7, [[0], 7, 27]],
+  [7, [4, 28], 14],
+  [[3], 10, 26, 7],
+];
+console.log(arr2.flat(4));
+
+//using recursion
+function flatten(arr2) {
+  const newArr = arr2.reduce((acc, curr) => {
+    if (typeof curr === 'object') {
+      return acc.concat(flatten(curr));
+    }
+    return acc.concat(curr);
+  }, []);
+  return newArr;
+}
+
+console.log(flatten(arr2));
